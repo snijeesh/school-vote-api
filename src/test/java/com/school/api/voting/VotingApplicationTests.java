d64ec4b5520b04424fa50395c3bdf7a5b8a3c91d
snijeesh <snijeesh@gmail.com>
2024-01-09 22:16:37 -0800

    Added new spring boot project files.

---
+++ src/test/java/com/school/api/voting/VotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.api.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
